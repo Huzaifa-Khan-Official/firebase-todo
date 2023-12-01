24db989d8de0e386199dae40f6343fa990d0b208
Huzaifa-Khan-Official <m.huzaifakhan2004@gmail.com>
2023-12-01 17:07:01 +0500

    done

---
+++ login/login/login.js
@@ -0,0 +1,106 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithPopup
+} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
+import {
+    getFirestore,
+    doc,
+    setDoc,
+  } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyABpF5Aq3pvwYfqSGd4A1LfyhjgKApnFmE",
+    authDomain: "test-project-2b.firebaseapp.com",
+    projectId: "test-project-2b",
+    storageBucket: "test-project-2b.appspot.com",
+    messagingSenderId: "545460196890",
+    appId: "1:545460196890:web:c8718f9b0776412ddaa3a3",
+    measurementId: "G-24X003ETZV"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+let lemail = document.querySelector("#lemail"); // get email to login user
+let lpassword = document.querySelector("#lpassword"); // get password to login user
+let lbtn = document.querySelector("#lbtn"); // get login btn
+let errorPara = document.querySelector("#errorPara"); // get error paragraph
+
+
+lbtn.addEventListener("click", () => {
+
+    signInWithEmailAndPassword(auth, lemail.value, lpassword.value)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            localStorage.setItem("userUid", user.uid)
+            location.href = "../index.html"
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = errorCode.slice(5).toUpperCase();
+            const errMessage = errorMessage.replace(/-/g, " ")
+            errorPara.innerText = errMessage;
+            setTimeout(() => {
+                errorPara.innerHTML = "";
+            }, 3000);
+        });
+})
+
+lpassword.addEventListener("keypress", (e) => {
+    if (e.key == "Enter") {
+        lbtn.click()
+    }
+})
+
+const googleSignInBtn = document.getElementById("googleSignInBtn");
+
+googleSignInBtn.addEventListener("click", () => {
+    signInWithPopup(auth, provider)
+        .then(async (result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+
+            const user = result.user;
+
+            let userData = {
+                sname: user.displayName,
+                semail: user.email,
+            };
+
+            await setDoc(doc(db, "users", user.uid), {
+                // collection name,   unique id of user
+                ...userData, // setting array in a database
+                userid: user.uid, // also user id in the database
+            });
+
+            localStorage.setItem("userUid", user.uid);
+
+            location.href = "../index.html";
+        })
+        .catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            // const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            console.log(errorMessage);
+            // if (email) {
+            //     errorPara.innerText = email;
+            //     setTimeout(() => {
+            //         errorPara.innerHTML = "";
+            //     }, 3000);
+            // }
+        });
+});
+
+if (localStorage.getItem("userUid")) {
+    location.href = "../index.html"
+}
