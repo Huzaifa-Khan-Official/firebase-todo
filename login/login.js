ba362f119805d210c1b0bb2f4a560078b8c4a0b1
Huzaifa-Khan-Official <m.huzaifakhan2004@gmial.com>
2023-12-03 18:08:00 +0500

    google signin done

--- login/login.js
+++ login/login.js
@@ -3,19 +3,29 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebas
 import {
     getAuth,
     signInWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithPopup
 } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
+import {
+    getFirestore,
+    doc,
+    setDoc,
+  } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDKbcJ3CVh0M6_MnGQjF2Iw_LmskUabrdE",
-    authDomain: "fir-todo-5ed69.firebaseapp.com",
-    projectId: "fir-todo-5ed69",
-    storageBucket: "fir-todo-5ed69.appspot.com",
-    messagingSenderId: "70260766093",
-    appId: "1:70260766093:web:5ec9b1faef82084c2df9c4"
-  };
+    apiKey: "AIzaSyABpF5Aq3pvwYfqSGd4A1LfyhjgKApnFmE",
+    authDomain: "test-project-2b.firebaseapp.com",
+    projectId: "test-project-2b",
+    storageBucket: "test-project-2b.appspot.com",
+    messagingSenderId: "545460196890",
+    appId: "1:545460196890:web:c8718f9b0776412ddaa3a3",
+    measurementId: "G-24X003ETZV"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
 
 let lemail = document.querySelector("#lemail"); // get email to login user
 let lpassword = document.querySelector("#lpassword"); // get password to login user
@@ -47,6 +57,49 @@ lpassword.addEventListener("keypress", (e) => {
         lbtn.click()
     }
 })
+
+const googleSignInBtn = document.getElementById("googleSignInBtn");
+
+googleSignInBtn.addEventListener("click", () => {
+    signInWithPopup(auth, provider)
+        .then(async (result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+
+            const user = result.user;
+
+            let userData = {
+                sname: user.displayName,
+                semail: user.email,
+            };
+
+            await setDoc(doc(db, "users", user.uid), {
+                // collection name,   unique id of user
+                ...userData, // setting array in a database
+                userid: user.uid, // also user id in the database
+            });
+
+            localStorage.setItem("userUid", user.uid);
+
+            location.href = "../index.html";
+        })
+        .catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            // const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            console.log(errorMessage);
+            // if (email) {
+            //     errorPara.innerText = email;
+            //     setTimeout(() => {
+            //         errorPara.innerHTML = "";
+            //     }, 3000);
+            // }
+        });
+});
 
 if (localStorage.getItem("userUid")) {
     location.href = "../index.html"
